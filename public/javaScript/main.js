22d6547f922193ed30308aa32da1e8722d88c85f
Ivan <Ivan_567.92@mail.ru>
2023-09-14 14:40:20 +0300

    process work slidery

--- public/javaScript/main.js
+++ public/javaScript/main.js
@@ -117,28 +117,113 @@ const $slide_items = document.querySelectorAll('.our-projects-slider__item')
 const $prev_slide = document.querySelector('.our-projects__arrow.prev-slide')
 const $next_slide = document.querySelector('.our-projects__arrow.next-slide')
 
+const $slider_switch = document.querySelectorAll('.our-projects-slider-switch__item')
+
+
 let count = 0;
 let width = 0;
 
-function currentSlide() {
+function nextSlide() {
     let currentElement;
+
     $slide_items.forEach((el, i) => {
         if (el.classList.contains('active')) {
             el.classList.remove('active')
             currentElement = el.nextElementSibling
         }
     })
+
     currentElement.classList.add('active');
 }
 
-$prev_slide.addEventListener('click', () => {
+function prevSlide() {
+    let currentElement;
+
+    $slide_items.forEach((el, i) => {
+        if (el.classList.contains('active')) {
+            el.classList.remove('active')
+            currentElement = el.previousElementSibling
+        }
+    })
+    currentElement.classList.add('active');
+}
+
+$next_slide.addEventListener('click', () => {
+    count++
     width = width + $slide_item.offsetWidth
-    console.log($slide_item.offsetWidth);
-    currentSlide()
+
+    if (count === $slide_items.length - 1) $next_slide.classList.add('hide')
+    if (count > 0) $prev_slide.classList.remove('hide')
+
+    $slider_switch.forEach((el) => {
+        el.classList.remove('active')
+    })
+    $slider_switch[count].classList.add('active')
+
+    nextSlide()
     rollSlider()
 })
 
+$prev_slide.addEventListener('click', () => {
+    count--
+    width = width - $slide_item.offsetWidth
 
+    if (count === 0) $prev_slide.classList.add('hide')
+    if (count < $slide_items.length - 1) $next_slide.classList.remove('hide')
+
+    $slider_switch.forEach((el) => {
+        el.classList.remove('active')
+    })
+    $slider_switch[count].classList.add('active')
+
+    prevSlide()
+    rollSlider()
+})
+
+$slide_items.forEach((el, i) => {
+    el.addEventListener('click', () => {
+        $slide_items.forEach((el) => {
+            el.classList.remove('active')
+        })
+        $slider_switch.forEach((el) => {
+            el.classList.remove('active')
+        })
+        $slider_switch[i].classList.add('active')
+        el.classList.add('active')
+        setCurrentWidtSlider(i)
+    })
+})
+
+$slider_switch.forEach((el, i) => {
+    el.addEventListener('click', () => {
+        $slider_switch.forEach((el) => {
+            el.classList.remove('active')
+        })
+        $slide_items.forEach((el) => {
+            el.classList.remove('active')
+        })
+        $slide_items[i].classList.add('active')
+        el.classList.add('active')
+        setCurrentWidtSlider(i)
+    })
+})
+
+function setCurrentWidtSlider(index) {
+    width = 0
+    count = index
+
+    if (count === $slide_items.length - 1) $next_slide.classList.add('hide')
+    if (count > 0) $prev_slide.classList.remove('hide')
+
+    if (count === 0) $prev_slide.classList.add('hide')
+    if (count < $slide_items.length - 1) $next_slide.classList.remove('hide')
+
+    for (let i = 0; i < count; i++) {
+        width = width + $slide_item.offsetWidth
+    }
+
+    rollSlider()
+}
 
 function rollSlider() {
     $slider.style.transform = 'translate(-' + width + 'px)'
