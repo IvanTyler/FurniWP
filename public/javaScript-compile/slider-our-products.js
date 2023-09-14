a9708170472a1589665331f6272d836b710c3bca
Ivan <Ivan_567.92@mail.ru>
2023-09-14 18:39:45 +0300

    process work slider

--- public/javaScript-compile/slider-our-products.js
+++ public/javaScript-compile/slider-our-products.js
@@ -7,6 +7,12 @@ const $next_slide = document.querySelector('.our-projects__arrow.next-slide')
 
 const $slider_switch = document.querySelectorAll('.our-projects-slider-switch__item')
 
+const $our_projects_videos = document.querySelectorAll('.our-projects-video')
+const $our_projects_open_modal = document.querySelectorAll('.our-projects-slider__button')
+
+const $modal_main = document.querySelector('.modal-main')
+const $close_modal_main = document.querySelector('.modal-main__close')
+
 
 let count = 0;
 let width = 0;
@@ -36,7 +42,7 @@ function prevSlide() {
     currentElement.classList.add('active');
 }
 
-$next_slide.addEventListener('click', () => {
+$next_slide?.addEventListener('click', () => {
     count++
     width = width + $slide_item.offsetWidth
 
@@ -52,7 +58,7 @@ $next_slide.addEventListener('click', () => {
     rollSlider()
 })
 
-$prev_slide.addEventListener('click', () => {
+$prev_slide?.addEventListener('click', () => {
     count--
     width = width - $slide_item.offsetWidth
 
@@ -68,7 +74,7 @@ $prev_slide.addEventListener('click', () => {
     rollSlider()
 })
 
-$slide_items.forEach((el, i) => {
+$slide_items?.forEach((el, i) => {
     el.addEventListener('click', () => {
         $slide_items.forEach((el) => {
             el.classList.remove('active')
@@ -82,7 +88,7 @@ $slide_items.forEach((el, i) => {
     })
 })
 
-$slider_switch.forEach((el, i) => {
+$slider_switch?.forEach((el, i) => {
     el.addEventListener('click', () => {
         $slider_switch.forEach((el) => {
             el.classList.remove('active')
@@ -117,3 +123,18 @@ function rollSlider() {
     $slider.style.transform = 'translate(-' + width + 'px)'
 }
 window.addEventListener('resize', rollSlider)
+
+$our_projects_open_modal?.forEach((el, i) => {
+    el.addEventListener('click', () => {
+        $our_projects_videos[i].style.display = 'block'
+        $modal_main.style.display = 'block'
+    })
+})
+
+$close_modal_main?.addEventListener('click', () => {
+    $our_projects_open_modal?.forEach((el, i) => {
+        $our_projects_videos[i].style.display = 'none'
+        $modal_main.style.display = 'none'
+    })
+})
+
