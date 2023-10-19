3f4ec4550caa4dab0d342a79387161b658bf579f
Ivan <Ivan_567.92@mail.ru>
2023-10-19 13:17:45 +0300

    process work wodals

--- public/javaScript/main.js
+++ public/javaScript/main.js
@@ -29,12 +29,18 @@ openInformationListItem($deliveryPrivacyPolicy)
 const $discuss_your_project_form = document.querySelector('.discuss-your-project-form')
 const $input_name_your_project = document.querySelector('#input-name-your-project')
 const $input_phone_your_project = document.querySelector('#input-phone-your-project')
-
 const $request_sent_choosing_us = document.querySelector('.request-sent.thanks-for-choosing-us')
+
+const $form_get_shopping = document.querySelector('.form-get-shopping-list-free')
+const $input_shopping_name = document.querySelector('.form-get-shopping-list-free__input.name')
+const $input_shopping_phone = document.querySelector('.form-get-shopping-list-free__input.phone')
+const $request_sent_happy_shopping = document.querySelector('.request-sent.happy-shopping')
+
+const $close_choosing_us = document.querySelector('#close-modal-choosing-us')
+const $close_happy_shopping = document.querySelector('#close-modal-happy-shopping')
+
 const $happy_shopping = document.querySelector('.request-sent.happy-shopping')
 
-
-const $request_sent_close = document.querySelector('.request-sent__close')
 
 const $make_request_form_black = document.querySelector('#make-a-request-form-black')
 const $make_request_form_white = document.querySelector('#make-a-request-form-white')
@@ -66,18 +72,43 @@ $discuss_your_project_form?.addEventListener('submit', (event) => {
         $input_name_your_project.classList.remove('error')
         $input_phone_your_project.classList.remove('error')
 
-
         $discuss_your_project_form.reset();
         $discuss_your_project_modal.style.display = 'none';
         $request_sent_choosing_us.style.display = 'flex';
 
-        console.log(formData);
         const application = {
             id: Date.now(),
             ...formData,
         }
 
-        console.log(application);
+    }
+})
+
+$form_get_shopping?.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const formData = Object.fromEntries(new FormData(event.target))
+
+    if (!$input_shopping_name.value) {
+        $input_shopping_name.classList.add('error')
+    }
+    if (!$input_shopping_phone.value) {
+        $input_shopping_phone.classList.add('error')
+    }
+
+    if (($input_shopping_name.value) && ($input_shopping_phone.value)) {
+        $input_shopping_name.classList.remove('error')
+        $input_shopping_phone.classList.remove('error')
+
+        $form_get_shopping.reset();
+        $modal_main.style.display = 'block'
+        $request_sent_happy_shopping.style.display = 'flex';
+
+        const application = {
+            id: Date.now(),
+            ...formData,
+        }
+
     }
 })
 
@@ -125,10 +156,8 @@ function requestDataUser(target, ...args) {
 
 }
 
-$request_sent_close?.addEventListener('click', () => {
-    $discuss_your_project_modal.style.display = 'flex'
-    $request_sent_choosing_us.style.display = 'none'
-})
+$close_choosing_us?.addEventListener('click', () => closeMainModal())
+$close_happy_shopping?.addEventListener('click', () => closeMainModal())
 const $header = document.querySelector('.header')
 
 const $modal_main = document.querySelector('.modal-main')
